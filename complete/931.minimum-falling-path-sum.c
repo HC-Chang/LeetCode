e2b5bf74455792396c6f82e82e51f7fa33b3fd35
1111-UX425 <student0804@gmail.com>
2022-12-14 00:32:03 +0800

    modify 931

--- complete/931.minimum-falling-path-sum.c
+++ complete/931.minimum-falling-path-sum.c
@@ -36,6 +36,11 @@ int minFallingPathSum(int **matrix, int matrixSize, int *matrixColSize)
         if (dp[matrixSize - 1][i] < min)
             min = dp[matrixSize - 1][i];
     }
+
+    for (int i = 0; i < matrixSize; i++)
+        free(dp[i]);
+    free(dp);
+    
     return min;
 }
 // @lc code=end
