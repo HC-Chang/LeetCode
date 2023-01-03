911e9ff1f1b99a6af43ba3617baa295a4451851b
1111-UX425 <student0804@gmail.com>
2023-01-03 22:22:13 +0800

    944

---
+++ complete/944.delete-columns-to-make-sorted.c
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=944 lang=c
+ *
+ * [944] Delete Columns to Make Sorted
+ */
+
+// @lc code=start
+int minDeletionSize(char **strs, int strsSize)
+{
+    int len = strlen(strs[0]);
+    for (int i = 1; i < strsSize; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if (strs[0][j] == -1)
+                continue;
+            if (strs[i][j] - strs[i - 1][j] < 0)
+                strs[0][j] = -1;
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (strs[0][i] == -1)
+            count++;
+    }
+    return count;
+}
+// @lc code=end
