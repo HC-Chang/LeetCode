e29fe0bfb5238a177902b7bf6b5c30f1bc7a5195
1111-UX425 <student0804@gmail.com>
2023-01-06 20:34:34 +0800

    217

---
+++ complete/217.contains-duplicate.c
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=217 lang=c
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+int sort(void *a, void *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
+bool containsDuplicate(int *nums, int numsSize)
+{
+    qsort(nums, numsSize, sizeof(int), sort);
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (nums[i] == nums[i - 1])
+            return true;
+    }
+    return false;
+}
+// @lc code=end
