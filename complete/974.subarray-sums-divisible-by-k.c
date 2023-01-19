e07681fee49536e3c0c2b21a6148b1ce3bf77131
1111-UX425 <student0804@gmail.com>
2023-01-19 22:06:59 +0800

    974

---
+++ complete/974.subarray-sums-divisible-by-k.c
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=974 lang=c
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+int subarraysDivByK(int *nums, int numsSize, int k)
+{
+    int *map = calloc(k, sizeof(int));
+    map[0] = 1;
+
+    int count = 0;
+    int r = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        r = ((r + nums[i]) % k + k) % k;
+        count += map[r];
+        map[r]++;
+    }
+    free(map);
+    return count;
+}
+// @lc code=end
+
+// 前缀和可能是負数
+// r = ((r + nums[i]) % k + k) % k;
