be8b14f5d0edbc6ab83173adb41ec8369bd44235
1111-UX425 <student0804@gmail.com>
2023-01-21 21:16:23 +0800

    230

---
+++ complete/230.kth-smallest-element-in-a-bst.c
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=230 lang=c
+ *
+ * [230] Kth Smallest Element in a BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+
+void push_arr(int val, int *index, int *arr)
+{
+    arr[(*index)++] = val;
+}
+void traverse(struct TreeNode *root, int *index, int *arr)
+{
+    if (!root)
+        return;
+    push_arr(root->val, index, arr);
+    traverse(root->left, index, arr);
+    traverse(root->right, index, arr);
+}
+
+int kthSmallest(struct TreeNode *root, int k)
+{
+    int index = 0;
+    int arr[10001] = {0};
+    traverse(root, &index, &arr);
+    qsort(&arr, index, sizeof(int), sort);
+    return arr[k - 1];
+}
+// @lc code=end
+
+// BST + sort
