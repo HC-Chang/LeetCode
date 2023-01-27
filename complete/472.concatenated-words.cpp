f876d33065aef310bf3e77fad1bd2c91da89254d
1111-UX425 <student0804@gmail.com>
2023-01-27 09:12:29 +0800

    472

---
+++ complete/472.concatenated-words.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=472 lang=cpp
+ *
+ * [472] Concatenated Words
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+class Solution
+{
+    bool dfs(const string &word, int length, vector<bool> &visited,
+             const unordered_set<string> &dictionary)
+    {
+        if (length == word.length())
+        {
+            return true;
+        }
+        if (visited[length])
+        {
+            return false;
+        }
+        visited[length] = true;
+        for (int i = word.length() - (length == 0 ? 1 : 0); i > length; --i)
+        {
+            if (dictionary.count(word.substr(length, i - length)) &&
+                dfs(word, i, visited, dictionary))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string> &words)
+    {
+        unordered_set<string> dictionary(words.begin(), words.end());
+        vector<string> answer;
+        for (const string &word : words)
+        {
+            vector<bool> visited(word.length());
+            if (dfs(word, 0, visited, dictionary))
+            {
+                answer.push_back(word);
+            }
+        }
+        return answer;
+    }
+};
+// @lc code=end
+
+// DP / DFS
+
+// Official Solution: DFS
