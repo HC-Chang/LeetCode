cf3c04a1a6cf68b1076179f9ab08c75f933e9013
1111-UX425 <student0804@gmail.com>
2023-02-03 00:27:08 +0800

    modify 844 - add stack solution

--- complete/844.backspace-string-compare.c
+++ complete/844.backspace-string-compare.c
@@ -6,38 +6,96 @@
 
 // @lc code=start
 
-char *m_str(char *s)
+// Solution 2: stack
+typedef struct
 {
-    int read = 0;
-    int write = 0;
-    while (s[read] != '\0')
-    {
-        if (s[read] != '#')
-        {
-            s[write] = s[read];
-            write++;
-        }
-        else
-        {
-            // read++;
-            if (write > 0)
-            {
-                write--;
-            }
-        }
-        read++;
-    }
+    int index;
+    char *c;
+} STACK;
 
-    s[write] = '\0';
-    
-    return s;
+void init_stack(int n, STACK *s)
+{
+    s->c = calloc(n, sizeof(char));
 }
 
-// 雙指標
+void free_stack(STACK *s)
+{
+    free(s->c);
+    free(s);
+}
+
+void push_stack(char input, STACK *s)
+{
+    if (input == '#')
+    {
+        if (s->index > 0)
+            --s->index;
+    }
+    else
+        s->c[s->index++] = input;
+}
+
 bool backspaceCompare(char *s, char *t)
 {
-    s = m_str(s);
-    t = m_str(t);
-    return strcmp(s, t) == 0;
+    STACK *s_stack = calloc(1, sizeof(STACK));
+    STACK *t_stack = calloc(1, sizeof(STACK));
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+
+    init_stack(s_len, s_stack);
+    init_stack(t_len, t_stack);
+
+    for (int i = 0; i < s_len; i++)
+        push_stack(s[i], s_stack);
+    for (int i = 0; i < t_len; i++)
+        push_stack(t[i], t_stack);
+
+    if (s_stack->index != t_stack->index)
+        return false;
+    for (int i = 0; i < s_stack->index; i++)
+    {
+        if (s_stack->c[i] != t_stack->c[i])
+            return false;
+    }
+    
+    free_stack(s_stack);
+    free_stack(t_stack);
+
+    return true;
 }
 // @lc code=end
+
+// Solution 1: double pointer
+// char *m_str(char *s)
+// {
+//     int read = 0;
+//     int write = 0;
+//     while (s[read] != '\0')
+//     {
+//         if (s[read] != '#')
+//         {
+//             s[write] = s[read];
+//             write++;
+//         }
+//         else
+//         {
+//             // read++;
+//             if (write > 0)
+//             {
+//                 write--;
+//             }
+//         }
+//         read++;
+//     }
+
+//     s[write] = '\0';
+
+//     return s;
+// }
+
+// bool backspaceCompare(char *s, char *t)
+// {
+//     s = m_str(s);
+//     t = m_str(t);
+//     return strcmp(s, t) == 0;
+// }
