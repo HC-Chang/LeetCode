b64eec0ca5aa4842b72ee6916feeb73dc7e1e654
1111-UX425 <student0804@gmail.com>
2023-02-05 21:02:50 +0800

    modify 76

--- complete/76.minimum-window-substring.c
+++ complete/76.minimum-window-substring.c
@@ -18,38 +18,32 @@ char *minWindow(char *s, char *t)
     int start = 0;
     int r_len = INT_MAX;
 
-    char c;
-
     //  A ~ z
     // 65 ~ 122
     int s_valids[58] = {0};
     int t_valids[58] = {0};
 
     for (int i = 0; i < t_len; i++)
-    {
         t_valids[t[i] - 'A']++;
-    }
+
     for (int i = 0; i < 58; i++)
     {
         if (t_valids[i] != 0)
-        {
             t_valid_count++;
-        }
     }
 
+    char c;
+    char d;
     while (right < s_len)
     {
-        char c = s[right];
+        c = s[right];
         right++;
         if (t_valids[c - 'A'])
         {
             s_valids[c - 'A']++;
             if (s_valids[c - 'A'] == t_valids[c - 'A'])
-            {
                 s_valid_count++;
-            }
         }
-
         while (s_valid_count == t_valid_count)
         {
             if (right - left < r_len)
@@ -58,23 +52,21 @@ char *minWindow(char *s, char *t)
                 r_len = right - left;
                 printf("%i %i\n", start, r_len);
             }
-            char d = s[left];
+            d = s[left];
             left++;
             if (t_valids[d - 'A'])
             {
                 if (s_valids[d - 'A'] == t_valids[d - 'A'])
-                {
                     s_valid_count--;
-                }
+
                 s_valids[d - 'A']--;
             }
         }
     }
 
     if (r_len == INT_MAX)
-    {
         return "";
-    }
+
     r = calloc(r_len + 1, sizeof(char));
     strncpy(r, s + start, r_len);
     return r;
