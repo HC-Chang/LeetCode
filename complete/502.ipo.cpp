a7a4a15fc893fb45e2b6c381f049dd765c314ff3
1111-UX425 <student0804@gmail.com>
2023-02-23 22:45:01 +0800

    502

---
+++ complete/502.ipo.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=502 lang=cpp
+ *
+ * [502] IPO
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+    {
+        int n = profits.size();
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; i++)
+            projects.emplace_back(capital[i], profits[i]);
+
+        sort(projects.begin(), projects.end());
+        priority_queue<int> q;
+        int ptr = 0;
+        for (int i = 0; i < k; i++)
+        {
+            while (ptr < n && projects[ptr].first <= w)
+                q.push(projects[ptr++].second);
+
+            if (q.empty())
+                break;
+
+            w += q.top();
+            q.pop();
+        }
+        return w;
+    }
+};
+// @lc code=end
+
+// Official Solution
