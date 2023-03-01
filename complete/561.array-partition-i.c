39f0b106679f797cc474944a03ef8817fc9317f9
1111-UX425 <student0804@gmail.com>
2023-03-01 21:33:51 +0800

    561

---
+++ complete/561.array-partition-i.c
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=561 lang=c
+ *
+ * [561] Sort an Array
+ */
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+int arrayPairSum(int *nums, int numsSize)
+{
+    qsort(nums, numsSize, sizeof(int), sort);
+    int r = 0;
+    for (int i = 0; i < (numsSize + 1 / 2); i += 2)
+        r += nums[i];
+    return r;
+}
+// @lc code=end
