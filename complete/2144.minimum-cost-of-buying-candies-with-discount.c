95f32ade82bbe7dc675c631133c9dfc18a6d25a9
1111-UX425 <student0804@gmail.com>
2023-03-01 23:28:11 +0800

    2144

---
+++ complete/2144.minimum-cost-of-buying-candies-with-discount.c
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=2144 lang=c
+ *
+ * [2144] Minimum Cost of Buying Candies With Discount
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(int *)b - *(int *)a; }
+int minimumCost(int *cost, int costSize)
+{
+    qsort(cost, costSize, sizeof(int), sort);
+    int sum = 0;
+    int i = 0;
+    while (i < costSize)
+    {
+        if (i == costSize - 1)
+            sum += cost[i];
+        else
+            sum += cost[i] + cost[i + 1];
+        i += 3;
+    }
+    return sum;
+}
+
+// @lc code=end
