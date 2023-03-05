e7a10be08b26f306adced486ca40dbacc57e56f9
1111-UX425 <student0804@gmail.com>
2023-03-05 12:19:27 +0800

    6307.6308

---
+++ complete/6307.pass-the-pillow.c
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=6307 lang=c
+ *
+ * [6307] Pass the Pillow
+ */
+
+// Difficulty:Easy
+
+// There are n people standing in a line labeled from 1 to n.
+// The first person in the line is holding a pillow initially.
+// Every second, the person holding the pillow passes it to the next person standing in the line.
+// Once the pillow reaches the end of the line, the direction changes,
+// and people continue passing the pillow in the opposite direction.
+
+// For example, once the pillow reaches the nth person they pass it to the n - 1th person,
+//  then to the n - 2th person and so on.
+// Given the two positive integers n and time,
+// return the index of the person holding the pillow after time seconds.
+
+// Constraints:
+// 2 <= n <= 1000
+// 1 <= time <= 1000
+
+// @lc code=start
+int passThePillow(int n, int time)
+{
+    if (time + 1 <= n)
+        return time + 1;
+
+    int r = (time + 1) % (n * 2 - 2);
+    if (r > n)
+        r = n - (r - n);
+    if (r == 0)
+        r = 2;
+    return r;
+}
+// @lc code=end
---
+++ complete/6308.kth-largest-sum-in-a-binary-tree.c
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=6308 lang=c
+ *
+ * [6308] Kth Largest Sum in a Binary Tree
+ */
+
+// Difficulty:Medium
+
+// You are given the root of a binary tree and a positive integer k.
+// The level sum in the tree is the sum of the values of the nodes that are on the same level.
+// Return the kth largest level sum in the tree (not necessarily distinct). If there are fewer than k levels in the tree, return -1.
+
+// Note that two nodes are on the same level if they have the same distance from the root.
+
+// Constraints:
+// The number of nodes in the tree is n.
+// 2 <= n <= 105
+// 1 <= Node.val <= 106
+// 1 <= k <= n
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int height(struct TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int l_height = height(root->left) + 1;
+    int r_height = height(root->right) + 1;
+    return l_height > r_height ? l_height : r_height;
+}
+
+void traverse(struct TreeNode *root, int level, long long *col)
+{
+    if (!root)
+        return;
+
+    col[level] += root->val;
+    traverse(root->left, level + 1, col);
+    traverse(root->right, level + 1, col);
+}
+
+int sort(long long *a, long long *b) { return *(long long *)b > *(long long *)a; }
+
+long long kthLargestLevelSum(struct TreeNode *root, int k)
+{
+    if (!root)
+        return 0;
+
+    int h = height(root);
+    if (k > h)
+        return -1;
+
+    long long *level_sum = calloc((h), sizeof(long long));
+    traverse(root, 0, level_sum);
+    qsort(level_sum, h, sizeof(long long), sort);
+    for (int i = 0; i < h; i++)
+        printf("%lld\n", level_sum[i]);
+    long long max_sum = level_sum[k - 1];
+    free(level_sum);
+    return max_sum;
+}
+
+// @lc code=end
