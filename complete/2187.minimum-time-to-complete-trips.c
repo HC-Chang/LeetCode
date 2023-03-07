ba2aa0b4d3e5a815fbf4db4e17ae94f70b937dbe
1111-UX425 <student0804@gmail.com>
2023-03-07 23:13:56 +0800

    2187

---
+++ complete/2187.minimum-time-to-complete-trips.c
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=2187 lang=c
+ *
+ * [2187] Minimum Time to Complete Trips
+ */
+
+// @lc code=start
+int if_complete(int *time, int timeSize, int totalTrips, long long max_t)
+{
+    long long count = 0;
+    for (int i = 0; i < timeSize; i++)
+        count += max_t / time[i];
+    return count >= totalTrips;
+}
+
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+
+long long minimumTime(int *time, int timeSize, int totalTrips)
+{
+    qsort(time, timeSize, sizeof(int), sort);
+    long long h = (long long)time[timeSize - 1] * totalTrips;
+    long long l = 1;
+    long long mid;
+    long long r = INT_MAX;
+    while (l <= h)
+    {
+        mid = l + (h - l) / 2;
+        if (if_complete(time, timeSize, totalTrips, mid))
+        {
+            r = mid;
+            h = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
+    return r;
+}
+// @lc code=end
+
+// Note: binary search
