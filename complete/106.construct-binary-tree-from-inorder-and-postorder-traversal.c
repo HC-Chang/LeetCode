0be73cdb9669410dfcff0ab12241fe331992ab9b
1111-UX425 <student0804@gmail.com>
2023-03-16 19:24:36 +0800

    106

---
+++ complete/106.construct-binary-tree-from-inorder-and-postorder-traversal.c
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=106 lang=c
+ *
+ * [106] Construct Binary Tree from Inorder and Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+typedef struct
+{
+    int *postorder;
+    int post_index;
+    int *inorder;
+} TREE_CONSTRUCTOR;
+
+struct TreeNode *make_tree(TREE_CONSTRUCTOR *t_c, int start, int end)
+{
+    if (start == end)
+        return NULL;
+
+    struct TreeNode *node = malloc(sizeof(struct TreeNode));
+    node->val = t_c->postorder[--t_c->post_index];
+
+    int i = start;
+    while (i < end && t_c->inorder[i] != node->val)
+        i++;
+
+    node->right = make_tree(t_c, i + 1, end);
+    node->left = make_tree(t_c, start, i);
+
+    return node;
+}
+
+struct TreeNode *buildTree(int *inorder, int inorderSize, int *postorder, int postorderSize)
+{
+    TREE_CONSTRUCTOR t_c;
+    t_c.postorder = postorder;
+    t_c.post_index = postorderSize;
+    t_c.inorder = inorder;
+    return make_tree(&t_c, 0, inorderSize);
+}
+
+// @lc code=end
