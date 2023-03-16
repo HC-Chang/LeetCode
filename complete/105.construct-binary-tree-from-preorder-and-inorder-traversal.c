8e4d64c90f8b9648d0e8bcea48e1dc91df25cd81
1111-UX425 <student0804@gmail.com>
2023-03-16 19:39:32 +0800

    105

---
+++ complete/105.construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=105 lang=c
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+typedef struct
+{
+    int *preorder;
+    int pre_index;
+    int pre_size;
+    int *inorder;
+} TREE_CONSTRUCTOR;
+
+struct TreeNode *make_tree(TREE_CONSTRUCTOR *t_c, int start, int end)
+{
+    if (start == end)
+        return NULL;
+
+    struct TreeNode *node = malloc(sizeof(struct TreeNode));
+    node->val = t_c->preorder[t_c->pre_index++];
+
+    int i = start;
+    while (i < end && t_c->inorder[i] != node->val)
+        i++;
+
+    node->left = make_tree(t_c, start, i);
+    node->right = make_tree(t_c, i + 1, end);
+
+    return node;
+}
+
+struct TreeNode *buildTree(int *preorder, int preorderSize, int *inorder, int inorderSize)
+{
+    TREE_CONSTRUCTOR t_c;
+
+    t_c.preorder = preorder;
+    t_c.pre_index = 0;
+    t_c.pre_size = preorderSize;
+    t_c.inorder = inorder;
+
+    return make_tree(&t_c, 0, inorderSize);
+}
+
+// @lc code=end
