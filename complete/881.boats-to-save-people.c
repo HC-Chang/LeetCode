77746a1a4d4057e70021c051fe88b7da9df2efbd
1111-UX425 <student0804@gmail.com>
2023-04-03 09:01:00 +0800

    881

---
+++ complete/881.boats-to-save-people.c
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=881 lang=c
+ *
+ * [881] Boats to Save People
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+int numRescueBoats(int *people, int peopleSize, int limit)
+{
+    qsort(people, peopleSize, sizeof(int), sort);
+    int left = 0;
+    int right = peopleSize - 1;
+    int boat_count = 0;
+    while (left <= right)
+    {
+        if (people[left] + people[right] <= limit)
+        {
+            left++;
+            right--;
+        }
+        else
+            right--;
+        boat_count++;
+    }
+    return boat_count;
+}
+
+// @lc code=end
