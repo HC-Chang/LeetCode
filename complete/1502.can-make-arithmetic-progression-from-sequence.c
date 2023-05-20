d064eec6eac16d905fae9928afef311df4e159a5
1111-UX425 <student0804@gmail.com>
2023-05-20 13:03:51 +0800

    1502

---
+++ complete/1502.can-make-arithmetic-progression-from-sequence.c
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1502 lang=c
+ *
+ * [1502] Can Make Arithmetic Progression From Sequence
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+bool canMakeArithmeticProgression(int *arr, int arrSize)
+{
+    qsort(arr, arrSize, sizeof(int), sort);
+    int dif = arr[1] - arr[0];
+    for (int i = 1; i < arrSize - 1; i++)
+    {
+        if ((arr[i + 1] - arr[i]) != dif)
+            return false;
+    }
+    return true;
+}
+// @lc code=end
