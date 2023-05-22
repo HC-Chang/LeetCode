ba2eb8041a68839c585606231adf7d62209754c4
1111-UX425 <student0804@gmail.com>
2023-05-22 21:01:59 +0800

    347

---
+++ complete/347.top-k-frequent-elements.c
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=347 lang=c
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct
+{
+    int index;
+    int count;
+} DATA;
+int sort(void *a, void *b) { return (*(DATA *)b).count - (*(DATA *)a).count; }
+int *topKFrequent(int *nums, int numsSize, int k, int *returnSize)
+{
+    *returnSize = k;
+    DATA *map = malloc((2 * 1e5 + 1) * sizeof(DATA));
+    for (int i = 0; i < 2 * 1e5 + 1; i++)
+    {
+        map[i].index = -1e5 + i;
+        map[i].count = 0;
+    }
+    for (int i = 0; i < numsSize; i++)
+        (map[(nums[i] + 100000)]).count += 1;
+    qsort(map, 2 * 1e5 + 1, sizeof(DATA), sort);
+    int *r = malloc(k * sizeof(int));
+    for (int i = 0; i < k; i++)
+        r[i] = map[i].index;
+
+    free(map);
+    return r;
+}
+// @lc code=end
