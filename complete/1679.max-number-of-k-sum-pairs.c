ac5a8595db1bca5a6dfebe2bfc2420b2b0949deb
1111-UX425 <student0804@gmail.com>
2023-06-06 20:17:11 +0800

    1679

---
+++ complete/1679.max-number-of-k-sum-pairs.c
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1679 lang=c
+ *
+ * [1679] Max Number of K-Sum Pairs
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
+int maxOperations(int *nums, int numsSize, int k)
+{
+    qsort(nums, numsSize, sizeof(int), sort);
+    int count = 0;
+    int left = 0, right = numsSize - 1;
+    while (left < right)
+    {
+        if (nums[left] + nums[right] == k)
+        {
+            count++;
+            left++;
+            right--;
+        }
+        else if (nums[left] + nums[right] < k)
+        {
+            left++;
+        }
+        else if (nums[left] + nums[right] > k)
+        {
+            right--;
+        }
+    }
+    return count;
+}
+// @lc code=end
