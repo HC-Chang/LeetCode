60c3b48f2a9059445b9116739eccfe97f6348926
1111-UX425 <student0804@gmail.com>
2023-06-18 23:22:49 +0800

    modify 209

--- complete/209.minimum-size-subarray-sum.c
+++ complete/209.minimum-size-subarray-sum.c
@@ -5,6 +5,30 @@
  */
 
 // @lc code=start
+// Solution 2:
+int minSubArrayLen(int target, int *nums, int numsSize)
+{
+    int left = 0, right = 0;
+    int tmp = 0;
+    int min_count = INT_MAX;
+    while (right < numsSize)
+    {
+        tmp += nums[right++];
+        while (tmp - nums[left] >= target)
+            tmp -= nums[left++];
+
+        if (tmp >= target && right - left < min_count)
+            min_count = right - left;
+    }
+    if (min_count == INT_MAX)
+        min_count = 0;
+    return min_count;
+}
+// @lc code=end
+
+// Note: Sliding Window
+
+// Solution 1:
 int minSubArrayLen(int target, int *nums, int numsSize)
 {
     int left = 0, right = 0;
@@ -24,6 +48,3 @@ int minSubArrayLen(int target, int *nums, int numsSize)
         return 0;
     return min_count;
 }
-// @lc code=end
-
-// Note: Sliding Window
