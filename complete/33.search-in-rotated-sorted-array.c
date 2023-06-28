7502339b1d3c73db01ae153f49c8b02e2e17c7f6
1111-UX425 <student0804@gmail.com>
2023-06-28 23:48:44 +0800

    modify 33

--- complete/33.search-in-rotated-sorted-array.c
+++ complete/33.search-in-rotated-sorted-array.c
@@ -5,19 +5,10 @@
  */
 
 // @lc code=start
-
+// Solution 2:
+// 檢視中間與右邊，判斷旋轉狀況
 int search(int *nums, int numsSize, int target)
 {
-    // NOTE Solution 1
-    // for (int i = 0; i < numsSize; i++)
-    // {
-    //     if (nums[i] == target)
-    //     {
-    //         return i;
-    //     }
-    // }
-
-    // NOTE Solution 2
     int left = 0;
     int right = numsSize - 1;
     int middle;
@@ -27,36 +18,37 @@ int search(int *nums, int numsSize, int target)
         middle = left + (right - left) / 2;
 
         if (nums[middle] == target)
-        {
             return middle;
-        }
 
         if (nums[middle] > nums[right])
         {
             if (target < nums[middle] && target >= nums[left])
-            {
                 right = middle - 1;
-            }
             else
-            {
                 left = middle + 1;
-            }
         }
         else
         {
             if (target > nums[middle] && target <= nums[right])
-            {
                 left = middle + 1;
-            }
             else
-            {
                 right = middle - 1;
-            }
         }
     }
 
     return -1;
 }
 // @lc code=end
-// NOTE Solution 2
-// 檢視中間與右邊，判斷旋轉狀況
+
+// Solution 1:
+// int search(int *nums, int numsSize, int target)
+// {
+//     for (int i = 0; i < numsSize; i++)
+//     {
+//         if (nums[i] == target)
+//         {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
