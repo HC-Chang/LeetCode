2bbad7b1bc2d5372f390c6f15c2d5cb62f05b2d1
1111-UX425 <student0804@gmail.com>
2023-07-01 21:11:59 +0800

    modify 206

--- complete/206.reverse-linked-list.c
+++ complete/206.reverse-linked-list.c
@@ -12,14 +12,30 @@
  *     struct ListNode *next;
  * };
  */
+// Solution 2:
 struct ListNode *reverseList(struct ListNode *head)
 {
-    if (head == NULL || head->next == NULL)
-        return head;
-    struct ListNode *last = reverseList(head->next);
-    head->next->next = head;
-    head->next = NULL;
-
-    return last;
+    struct ListNode *prev = NULL;
+    struct ListNode *cur = head;
+    struct ListNode *nxt;
+    while (cur)
+    {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    return prev;
 }
 // @lc code=end
+
+// Solution 1:
+
+// struct ListNode* reverseList(struct ListNode* head){
+//     if(!head || !head->next)
+//         return head;
+//     struct ListNode* last = reverseList(head->next);
+//     head->next->next= head;
+//     head->next= NULL;
+//     return last;
+// }
