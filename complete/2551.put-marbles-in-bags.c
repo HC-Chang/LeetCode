49d014a95017a1c57520a1d218cb15c4af8805b8
1111-UX425 <student0804@gmail.com>
2023-07-08 10:25:05 +0800

    2551

---
+++ complete/2551.put-marbles-in-bags.c
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=2551 lang=c
+ *
+ * [2551]  Put Marbles in Bags
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(long long *)a - *(long long *)b; }
+long long putMarbles(int *weights, int weightsSize, int k)
+{
+    if (weightsSize == 1)
+        return 0;
+
+    long long *arr = malloc((weightsSize - 1) * sizeof(long long));
+    for (int i = 0; i < weightsSize - 1; i++)
+        arr[i] = weights[i] + weights[i + 1];
+    qsort(arr, weightsSize - 1, sizeof(long long), sort);
+
+    long long r = 0;
+    for (int i = 0; i < k - 1; i++)
+    {
+        r += arr[weightsSize - 2 - i];
+        r -= arr[i];
+    }
+
+    free(arr);
+    return r;
+}
+// @lc code=end
