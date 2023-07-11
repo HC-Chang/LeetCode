020aa2a5e0731156a1d20658c9f952a7c29a3fd8
1111-UX425 <student0804@gmail.com>
2023-07-12 00:05:28 +0800

    modify 17

--- complete/17.letter-combinations-of-a-phone-number.c
+++ complete/17.letter-combinations-of-a-phone-number.c
@@ -10,12 +10,12 @@
  */
 char phoneMap[10][5] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-void backtrack(int index, int digits_size, char *tmp_str, int *str_index, char *str, int *arr_size, char **arr)
+void backtrack(int index, int digits_size, char *tmp_str, char *str, int *arr_size, char **arr)
 {
     if (index == digits_size)
     {
-        char *tmp = malloc(sizeof(char) * (*str_index + 1));
-        memcpy(tmp, str, sizeof(char) * (*str_index + 1));
+        char *tmp = malloc(sizeof(char) * (digits_size + 1));
+        memcpy(tmp, str, sizeof(char) * (digits_size + 1));
         arr[(*arr_size)++] = tmp;
     }
     else
@@ -25,10 +25,9 @@ void backtrack(int index, int digits_size, char *tmp_str, int *str_index, char *
         int len = strlen(letters);
         for (int i = 0; i < len; i++)
         {
-            str[(*str_index)++] = letters[i];
-            str[(*str_index)] = 0;
-            backtrack(index + 1, digits_size, tmp_str, str_index, str, arr_size, arr);
-            str[--(*str_index)] = 0;
+            str[index] = letters[i];
+            str[index + 1] = 0;
+            backtrack(index + 1, digits_size, tmp_str, str, arr_size, arr);
         }
     }
 }
@@ -36,14 +35,10 @@ void backtrack(int index, int digits_size, char *tmp_str, int *str_index, char *
 char **letterCombinations(char *digits, int *returnSize)
 {
     int digits_size = strlen(digits);
+    *returnSize = 0;
     if (digits_size == 0)
-    {
-        *returnSize = 0;
         return NULL;
-    }
 
-    int arr_size = 0;
-    int str_index = 0;
     char *tmp_str = digits;
     int num = 1;
     for (int i = 0; i < digits_size; i++)
@@ -51,10 +46,12 @@ char **letterCombinations(char *digits, int *returnSize)
 
     char **arr = malloc(sizeof(char *) * num);
     char *str = malloc(sizeof(char *) * digits_size);
-    backtrack(0, digits_size, tmp_str, &str_index, str, &arr_size, arr);
-    *returnSize = arr_size;
+    backtrack(0, digits_size, tmp_str, str, returnSize, arr);
+    free(str);
     return arr;
 }
 // @lc code=end
 
 // Note: backtracking
+
+// FIXME index / str_index
