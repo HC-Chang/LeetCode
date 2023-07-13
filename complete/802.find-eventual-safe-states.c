b618720c2b2e301317b2884ffe78f78562a76a12
1111-UX425 <student0804@gmail.com>
2023-07-14 00:32:41 +0800

    modify 802

--- complete/802.find-eventual-safe-states.c
+++ complete/802.find-eventual-safe-states.c
@@ -13,19 +13,19 @@ void push_arr(int val, int *index, int *arr)
     arr[(*index)++] = val;
 }
 
-bool helper(int **graph, int *graphColSize, int cur, int *color)
+bool helper(int **graph, int *graphColSize, int cur, int *status)
 {
-    if (color[cur] > 0)
-        return color[cur] == 2;
-    color[cur] = 1;
+    if (status[cur] > 0)
+        return status[cur] == 2;
+    status[cur] = 1;
     for (int i = 0; i < graphColSize[cur]; i++)
     {
-        if (color[graph[cur][i]] == 2)
+        if (status[graph[cur][i]] == 2)
             continue;
-        if (color[graph[cur][i]] == 1 || !helper(graph, graphColSize, graph[cur][i], color))
+        if (status[graph[cur][i]] == 1 || !helper(graph, graphColSize, graph[cur][i], status))
             return false;
     }
-    color[cur] = 2;
+    status[cur] = 2;
     return true;
 }
 
@@ -33,14 +33,14 @@ int *eventualSafeNodes(int **graph, int graphSize, int *graphColSize, int *retur
 {
     *returnSize = 0;
     int *res = calloc(graphSize, sizeof(int));
-    int *color = calloc(graphSize, sizeof(int));
+    int *status = calloc(graphSize, sizeof(int));
     for (int i = 0; i < graphSize; ++i)
     {
-        if (helper(graph, graphColSize, i, color))
+        if (helper(graph, graphColSize, i, status))
             push_arr(i, returnSize, res);
     }
 
-    free(color);
+    free(status);
     return res;
 }
 // @lc code=end
