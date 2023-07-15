fc1e2314e011e93fd30434bc24a803f0b4680048
1111-UX425 <student0804@gmail.com>
2023-07-15 09:38:56 +0800

    78

---
+++ complete/78.subsets.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> arr;
+        vector<int> element;
+        int n = nums.size();
+        function<void(int)> dfs = [&](int i)
+        {
+            if (i == n)
+            {
+                arr.emplace_back(element);
+                return;
+            }
+            dfs(i + 1);
+            element.push_back(nums[i]);
+            dfs(i + 1);
+            element.pop_back();
+        };
+        dfs(0);
+        return arr;
+    }
+};
+// @lc code=end
