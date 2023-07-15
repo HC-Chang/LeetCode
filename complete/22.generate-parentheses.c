70ad266b68e9a88d4930bf0cdf5489ecd9f01229
1111-UX425 <student0804@gmail.com>
2023-07-15 18:43:33 +0800

    modify 22

--- complete/22.generate-parentheses.c
+++ complete/22.generate-parentheses.c
@@ -11,7 +11,7 @@
 void push_arr(char c, int i, int *arr_index, char *arr)
 {
     arr[i] = c;
-    *arr_index = strlen(arr);
+    *arr_index = i + 1;
 }
 
 void push_arrs(int *arr_index, char *arr, int *arrs_index, char **arrs)
@@ -45,7 +45,7 @@ char **generateParenthesis(int n, int *returnSize)
     char *arr = calloc(n * 2 + 1, sizeof(char));
     int arr_index = 0;
     *returnSize = 0;
-    char **arrs = malloc(50000 * sizeof(char *));
+    char **arrs = malloc(2000 * sizeof(char *));
 
     dfs(0, 0, n, &arr_index, arr, returnSize, arrs);
 
