c3a407f18c9dff02810e9b45bc381c79ffc46fa6
1111-UX425 <student0804@gmail.com>
2023-07-25 21:40:15 +0800

    852

---
+++ complete/852.peak-index-in-a-mountain-array.c
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=852 lang=c
+ *
+ * [852] Peak Index in a Mountain Array
+ */
+
+// @lc code=start
+// Solution 2: binary search
+int peakIndexInMountainArray(int *arr, int arrSize)
+{
+    int left = 0;
+    int right = arrSize - 1;
+    int mid;
+    while (left < right)
+    {
+        mid = left + (right - left) / 2;
+        if (arr[mid] < arr[mid + 1])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    mid = left + (right - left) / 2;
+    return mid;
+}
+// @lc code=end
+
+// Solution 1:
+// typedef struct
+// {
+//     int index;
+//     int val;
+// } DATA;
+
+// int sort(void *a, void *b) { return ((DATA *)b)->val - ((DATA *)a)->val; }
+
+// int peakIndexInMountainArray(int *arr, int arrSize)
+// {
+//     DATA *d = malloc(arrSize * sizeof(DATA));
+//     for (int i = 0; i < arrSize; i++)
+//     {
+//         d[i].index = i;
+//         d[i].val = arr[i];
+//     }
+//     qsort(d, arrSize, sizeof(d), sort);
+//     int r = d[0].index;
+//     free(d);
+//     return r;
+// }
