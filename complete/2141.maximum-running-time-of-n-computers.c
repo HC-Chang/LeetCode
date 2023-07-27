a7a78d3185266c365cc57d540eaa8831c66ea939
1111-UX425 <student0804@gmail.com>
2023-07-28 00:13:02 +0800

    2141

---
+++ complete/2141.maximum-running-time-of-n-computers.c
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2141 lang=c
+ *
+ * [2141] Maximum Running Time of N Computers
+ */
+
+// @lc code=start
+int sort(void *a, void *b) { return *(int *)b - *(int *)a; }
+long long maxRunTime(int n, int *batteries, int batteriesSize)
+{
+    if (batteriesSize < n)
+        return 0;
+    qsort(batteries, batteriesSize, sizeof(int), sort);
+    long long sum = 0;
+    for (int i = 0; i < batteriesSize; i++)
+        sum += batteries[i];
+    for (int i = 0; i < batteriesSize; i++)
+    {
+        if (batteries[i] <= sum / n)
+            break;
+        sum -= batteries[i];
+        --n;
+    }
+    return sum / n;
+}
+// @lc code=end
