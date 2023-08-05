718de6288df7e08348675801dd51d43502dc0413
1111-UX425 <student0804@gmail.com>
2023-08-05 18:26:39 +0800

    modify 1143

--- complete/1143.longest-common-subsequence.c
+++ complete/1143.longest-common-subsequence.c
@@ -5,30 +5,61 @@
  */
 
 // @lc code=start
+// Solution 2:
 int longestCommonSubsequence(char *text1, char *text2)
 {
     int len1 = strlen(text1);
     int len2 = strlen(text2);
 
-    int **dp = calloc(len1 + 1, sizeof(int *));
-    for (int i = 0; i <= len1; i++)
-        dp[i] = calloc(len2 + 1, sizeof(int));
+    int *dp = calloc(len2 + 1, sizeof(int));
 
+    int prev;
+    int tmp;
     for (int i = 1; i <= len1; i++)
     {
+        prev = dp[0];
         for (int j = 1; j <= len2; j++)
         {
+            tmp = dp[j];
+
             if (text1[i - 1] == text2[j - 1])
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+                dp[j] = prev + 1;
             else
-                dp[i][j] = fmax(dp[i - 1][j], dp[i][j - 1]);
+                dp[j] = fmax(dp[j - 1], dp[j]);
+            prev = tmp;
         }
     }
-    int r = dp[len1][len2];
-    for (int i = 0; i <= len1; i++)
-        free(dp[i]);
+    int r = dp[len2];
     free(dp);
 
     return r;
 }
 // @lc code=end
+
+// Solution 1:
+// int longestCommonSubsequence(char *text1, char *text2)
+// {
+//     int len1 = strlen(text1);
+//     int len2 = strlen(text2);
+
+//     int **dp = calloc(len1 + 1, sizeof(int *));
+//     for (int i = 0; i <= len1; i++)
+//         dp[i] = calloc(len2 + 1, sizeof(int));
+
+//     for (int i = 1; i <= len1; i++)
+//     {
+//         for (int j = 1; j <= len2; j++)
+//         {
+//             if (text1[i - 1] == text2[j - 1])
+//                 dp[i][j] = dp[i - 1][j - 1] + 1;
+//             else
+//                 dp[i][j] = fmax(dp[i - 1][j], dp[i][j - 1]);
+//         }
+//     }
+//     int r = dp[len1][len2];
+//     for (int i = 0; i <= len1; i++)
+//         free(dp[i]);
+//     free(dp);
+
+//     return r;
+// }
