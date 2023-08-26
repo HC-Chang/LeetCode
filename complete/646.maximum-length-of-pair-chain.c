57110639c50ed22d6b453fce3b795157a357522e
1111-UX425 <student0804@gmail.com>
2023-08-26 20:11:14 +0800

    646

---
+++ complete/646.maximum-length-of-pair-chain.c
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=646 lang=c
+ *
+ * [646] Maximum Length of Pair Chain
+ */
+
+// @lc code=start
+int sort(void *a, void *b)
+{
+    int **x = (int **)a;
+    int **y = (int **)b;
+    return x[0][1] - y[0][1];
+}
+
+int findLongestChain(int **pairs, int pairsSize, int *pairsColSize)
+{
+    qsort(pairs, pairsSize, sizeof(int *), sort);
+
+    int last = INT_MIN;
+    int count = 0;
+    for (int i = 0; i < pairsSize; i++)
+    {
+        if (pairs[i][0] > last)
+        {
+            count++;
+            last = pairs[i][1];
+        }
+    }
+
+    return count;
+}
+// @lc code=end
+
+// Note: DP . greedy . sorting
