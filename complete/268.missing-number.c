2260b87192a34f4f53696b972cfe1232f2fcb903
1111-UX425 <student0804@gmail.com>
2023-10-01 21:29:43 +0800

    modify 268

--- complete/268.missing-number.c
+++ complete/268.missing-number.c
@@ -5,18 +5,28 @@
  */
 
 // @lc code=start
+// Solution 2: bit manipulation
 int missingNumber(int *nums, int numsSize)
 {
-    int hash[numsSize + 1];
-    memset(hash, 0, (numsSize + 1) * sizeof(int));
+    int r = numsSize;
     for (int i = 0; i < numsSize; i++)
-        ++hash[nums[i]];
-    int i;
-    for (i = 0; i <= numsSize; i++)
-    {
-        if (hash[i] == 0)
-            break;
-    }
-    return i;
+        r ^= i ^ nums[i];
+    return r;
 }
 // @lc code=end
+
+// Solution 1:
+// int missingNumber(int *nums, int numsSize)
+// {
+//     int hash[numsSize + 1];
+//     memset(hash, 0, (numsSize + 1) * sizeof(int));
+//     for (int i = 0; i < numsSize; i++)
+//         ++hash[nums[i]];
+//     int i;
+//     for (i = 0; i <= numsSize; i++)
+//     {
+//         if (hash[i] == 0)
+//             break;
+//     }
+//     return i;
+// }
