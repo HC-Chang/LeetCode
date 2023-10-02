d1bd9af8e431f3f2e8b5fed5d955f699a23e48e9
1111-UX425 <student0804@gmail.com>
2023-10-02 22:06:45 +0800

    2038

---
+++ complete/2038.remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=2038 lang=cpp
+ *
+ * [2038] Remove Colored Pieces if Both Neighbors are the Same Color
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool winnerOfGame(string colors)
+    {
+        int len = colors.size();
+        if (len < 3)
+            return false;
+        int cnt[2] = {0};
+        int tmp = 1;
+        char c = colors[0];
+        for (int i = 1; i < len; i++)
+        {
+            if (colors[i] == c)
+                tmp++;
+            else
+            {
+                if (tmp > 2)
+                {
+                    if (c == 'B')
+                        cnt[1] += tmp - 2;
+                    else
+                        cnt[0] += tmp - 2;
+                }
+                tmp = 1;
+                c = colors[i];
+            }
+        }
+        if (tmp > 2)
+        {
+            if (c == 'B')
+                cnt[1] += tmp - 2;
+            else
+                cnt[0] += tmp - 2;
+        }
+        return cnt[0] > cnt[1];
+    }
+};
+// @lc code=end
