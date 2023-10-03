7a4666435b9187becf2745cbb0e43ac9a435045d
1111-UX425 <student0804@gmail.com>
2023-10-03 20:46:05 +0800

    modify 151

--- complete/151.reverse-words-in-a-string.c
+++ complete/151.reverse-words-in-a-string.c
@@ -5,46 +5,86 @@
  */
 
 // @lc code=start
-
-void find_last_letter(char *s, int *start_index, int *len)
+// Solution 2:
+char *reverseWords(char *s)
 {
-    *start_index = 0;
-    *len = 0;
-    int start = 0;
     int s_len = strlen(s);
-    for (int i = 0; i < s_len; i++)
+    char *r = calloc(s_len + 1, sizeof(char));
+    int end_idx;
+    int en = 0;
+    int cnt = 0;
+    for (int i = s_len - 1; i >= 0; i--)
     {
-        if (s[i] != ' ')
+        if (s[i] == ' ')
         {
-            if (start)
-                *len += 1;
-            else
+            if (en == 1)
             {
-                start = 1;
-                *len = 1;
-                *start_index = i;
+                strncat(r, s + i + 1, end_idx - i);
+                strncat(r, " ", 1);
+                cnt += end_idx - i + 1;
+                en = 0;
             }
         }
         else
-            start = 0;
+        {
+            if (en == 0)
+            {
+                en = 1;
+                end_idx = i;
+            }
+        }
     }
-}
-
-char *reverseWords(char *s)
-{
-    char *arr = calloc(strlen(s) + 2, sizeof(char));
-    int start_index = 0;
-    int len = 0;
-    while (*s)
+    if (en == 1 && s[0] != ' ')
     {
-        find_last_letter(s, &start_index, &len);
-        strncat(arr, s + start_index, len);
-        s[start_index] = '\0';
-        arr[strlen(arr)] = ' ';
+        strncat(r, s, end_idx + 1);
+        cnt += end_idx + 1;
     }
-    while (arr[strlen(arr) - 1] == ' ')
-        arr[strlen(arr) - 1] = '\0';
-
-    return arr;
+    while (r[cnt - 1] == ' ')
+        cnt--;
+    r[cnt] = '\0';
+    return r;
 }
 // @lc code=end
+
+// Solution 1:
+// void find_last_letter(char *s, int *start_index, int *len)
+// {
+//     *start_index = 0;
+//     *len = 0;
+//     int start = 0;
+//     int s_len = strlen(s);
+//     for (int i = 0; i < s_len; i++)
+//     {
+//         if (s[i] != ' ')
+//         {
+//             if (start)
+//                 *len += 1;
+//             else
+//             {
+//                 start = 1;
+//                 *len = 1;
+//                 *start_index = i;
+//             }
+//         }
+//         else
+//             start = 0;
+//     }
+// }
+
+// char *reverseWords(char *s)
+// {
+//     char *arr = calloc(strlen(s) + 2, sizeof(char));
+//     int start_index = 0;
+//     int len = 0;
+//     while (*s)
+//     {
+//         find_last_letter(s, &start_index, &len);
+//         strncat(arr, s + start_index, len);
+//         s[start_index] = '\0';
+//         arr[strlen(arr)] = ' ';
+//     }
+//     while (arr[strlen(arr) - 1] == ' ')
+//         arr[strlen(arr) - 1] = '\0';
+
+//     return arr;
+// }
