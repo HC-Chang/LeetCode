a12a8277f0ab8eb276c146191422d7dcc08e92c1
1111-UX425 <student0804@gmail.com>
2023-10-21 23:15:35 +0800

    1425

---
+++ complete/1425.constrained-subsequence-sum.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1425 lang=cpp
+ *
+ * [1425] Constrained Subsequence Sum
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int constrainedSubsetSum(vector<int> &nums, int k)
+    {
+        deque<int> queue;
+        vector<int> dp(nums.size());
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (!queue.empty() && i - queue.front() > k)
+            {
+                queue.pop_front();
+            }
+
+            dp[i] = (!queue.empty() ? dp[queue.front()] : 0) + nums[i];
+            while (!queue.empty() && dp[queue.back()] < dp[i])
+            {
+                queue.pop_back();
+            }
+
+            if (dp[i] > 0)
+            {
+                queue.push_back(i);
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+// @lc code=end
