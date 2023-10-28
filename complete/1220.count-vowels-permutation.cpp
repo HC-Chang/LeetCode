f1ee427e1b5c2ebb7dba50d4a5f7c4ebb6f9a604
1111-UX425 <student0804@gmail.com>
2023-10-28 09:51:56 +0800

    1220

---
+++ complete/1220.count-vowels-permutation.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1220 lang=cpp
+ *
+ * [1220] Count Vowels Permutation
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countVowelPermutation(int n)
+    {
+        unsigned int dp[5];
+        unsigned int tmp[5];
+        int mod = 1e9 + 7;
+        for (int i = 0; i < 5; i++)
+            dp[i] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            tmp[0] = (dp[1]) % mod;
+            tmp[1] = (dp[0] + dp[2]) % mod;
+            tmp[2] = (dp[0] + dp[1] + dp[3] + dp[4]) % mod;
+            tmp[3] = (dp[2] + dp[4]) % mod;
+            tmp[4] = (dp[0]) % mod;
+            memcpy(dp, tmp, sizeof(dp));
+        }
+        unsigned int sum = 0;
+        sum = accumulate(dp, dp + 5, sum);
+        return sum % mod;
+    }
+};
+// @lc code=end
+
+// Note: DP
