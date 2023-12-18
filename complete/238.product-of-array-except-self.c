64f332b510ac2790174c2acb745abafacd18bc5b
Ted Chang <student0804@gmail.com>
2023-12-18 23:17:05 +0800

    modify 238

--- complete/238.product-of-array-except-self.c
+++ complete/238.product-of-array-except-self.c
@@ -11,46 +11,46 @@
  */
 int *productExceptSelf(int *nums, int numsSize, int *returnSize)
 {
-    int *r = calloc(numsSize, sizeof(int));
     *returnSize = numsSize;
 
-    // NOTE Solution 1:
+    int product = 1;
     int zero_count = 0;
-    int products = 1;
-    int zero_index = -1;
     for (int i = 0; i < numsSize; i++)
-
-    // NOTE Solution 2:
-    // int zero_count = nums[0] == 0 ? 1 : 0;
-    // int products = nums[0] == 0 ? 0 : nums[0];
-    // int zero_index = nums[0] == 0 ? 0 : -1;
-    // for (int i = 1; i < numsSize; i++)
     {
         if (nums[i] == 0)
-        {
-            if (zero_count == 0)
-            {
-                zero_index = i;
-            }
-            zero_count++;
-            continue;
-        }
-        products *= nums[i];
+            ++zero_count;
+        else
+            product *= nums[i];
     }
 
-    if (zero_count == 0)
+    if (zero_count >= 2)
+    {
+        for (int i = 0; i < numsSize; i++)
+            nums[i] = 0;
+        return nums;
+    }
+
+    if (zero_count == 1)
     {
         for (int i = 0; i < numsSize; i++)
         {
-            r[i] = products / nums[i];
+            if (nums[i] == 0)
+                nums[i] = product;
+            else
+                nums[i] = 0;
         }
-    }
-    else if (zero_count > 0 && zero_count < 2)
-    {
-        r[zero_index] = products;
+        return nums;
     }
 
-    return r;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] == 0)
+            nums[i] = product;
+        else
+            nums[i] = product / nums[i];
+    }
+
+    return nums;
 }
 // @lc code=end
 // NOTE 考慮 0 出現的次數
