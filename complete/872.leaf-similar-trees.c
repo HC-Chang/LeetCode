41f1c272b287711ed61ab97ac9bd0920b6965b78
Ted Chang <student0804@gmail.com>
2023-12-20 22:13:15 +0800

    modify 872

--- complete/872.leaf-similar-trees.c
+++ complete/872.leaf-similar-trees.c
@@ -13,34 +13,33 @@
  *     struct TreeNode *right;
  * };
  */
-void last(struct TreeNode *root, int *arr, int *index)
+void record_leaf(struct TreeNode *root, int *idx, int *arr)
 {
     if (!root)
         return;
-    if (root->left)
-        last(root->left, arr, index);
-    if (root->right)
-        last(root->right, arr, index);
-    if (!root->left && !root->right)
-        arr[++*index] = root->val;
-}
 
+    if (!root->left && !root->right)
+    {
+        arr[(*idx)++] = root->val;
+        return;
+    }
+
+    if (root->left)
+        record_leaf(root->left, idx, arr);
+    if (root->right)
+        record_leaf(root->right, idx, arr);
+}
 bool leafSimilar(struct TreeNode *root1, struct TreeNode *root2)
 {
-    if (!root1 && !root2)
-        return true;
-    if ((root1 == NULL && root2 != NULL) || (root1 != NULL && root2 == NULL))
-        return false;
-
     int arr1[200] = {0};
     int arr2[200] = {0};
-    int index1 = 0;
-    int index2 = 0;
-    last(root1, arr1, &index1);
-    last(root2, arr2, &index2);
-
-    if (index1 != index2)
+    int idx_1 = 0;
+    int idx_2 = 0;
+    record_leaf(root1, &idx_1, arr1);
+    record_leaf(root2, &idx_2, arr2);
+    if (idx_1 != idx_2)
         return false;
-    return !memcmp(arr1, arr2, 200*sizeof(int));
+
+    return !memcmp(arr1, arr2, sizeof(arr1));
 }
 // @lc code=end
