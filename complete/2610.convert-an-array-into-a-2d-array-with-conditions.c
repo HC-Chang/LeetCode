acf7d9fc14a6ded6bcd182ec7b3931185c0ac5fd
Ted Chang <student0804@gmail.com>
2024-01-02 23:31:46 +0800

    modify 2610

--- complete/2610.convert-an-array-into-a-2d-array-with-conditions.c
+++ complete/2610.convert-an-array-into-a-2d-array-with-conditions.c
@@ -10,31 +10,66 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int **findMatrix(int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
-{
-    int hash[201] = {0};
+// Solution 2:
+int sort(void *a ,void *b){return *(int*)a - *(int *)b;}
+int** findMatrix(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    qsort(nums, numsSize, sizeof(int), sort);
+    int **ans = malloc(numsSize*sizeof(int*));
+    for(int i =0;i<numsSize; i++)
+        ans[i] = calloc(numsSize,sizeof(int));
+    
+    int cnt = 1;
     *returnSize = 0;
-    for (int i = 0; i < numsSize; i++)
+    returnColumnSizes[0] = calloc(numsSize, sizeof(int)) ;
+    for(int i = 1 ; i<numsSize; i++)
     {
-        if (++hash[nums[i]] > *returnSize)
-            *returnSize = hash[nums[i]];
-    }
-
-    returnColumnSizes[0] = calloc(*returnSize, sizeof(int));
-    int **ans = malloc(*returnSize * sizeof(int *));
-    for (int i = 0; i < *returnSize; i++)
-        ans[i] = calloc(200, sizeof(int));
-    for (int i = 1; i < 201; i++)
-    {
-        if (hash[i] > 0)
+        if(nums[i] == nums[i-1])
         {
-            for (int j = 0; j < hash[i]; j++)
-                ans[j][returnColumnSizes[0][j]++] = i;
+            cnt++;
+            if(cnt > *returnSize)
+                *returnSize = cnt;
+        }
+        else
+        {
+            for(int j = 0; j<cnt; j++)
+                ans[j][returnColumnSizes[0][j]++] = nums[i-1];
+            cnt = 1;
         }
     }
+    if(cnt > *returnSize)
+        *returnSize = cnt;
+    for(int j = 0; j<cnt; j++)
+        ans[j][returnColumnSizes[0][j]++] = nums[numsSize-1];
 
     return ans;
 }
 // @lc code=end
 
 // Note: hash table
+
+// Solution 1:
+// int **findMatrix(int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
+// {
+//     int hash[201] = {0};
+//     *returnSize = 0;
+//     for (int i = 0; i < numsSize; i++)
+//     {
+//         if (++hash[nums[i]] > *returnSize)
+//             *returnSize = hash[nums[i]];
+//     }
+
+//     returnColumnSizes[0] = calloc(*returnSize, sizeof(int));
+//     int **ans = malloc(*returnSize * sizeof(int *));
+//     for (int i = 0; i < *returnSize; i++)
+//         ans[i] = calloc(200, sizeof(int));
+//     for (int i = 1; i < 201; i++)
+//     {
+//         if (hash[i] > 0)
+//         {
+//             for (int j = 0; j < hash[i]; j++)
+//                 ans[j][returnColumnSizes[0][j]++] = i;
+//         }
+//     }
+
+//     return ans;
+// }
