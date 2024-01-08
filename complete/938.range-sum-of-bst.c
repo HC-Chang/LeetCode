c2f2a93bf0f116b2b93dec8371e5aab7ef431268
Ted Chang <student0804@gmail.com>
2024-01-08 22:00:31 +0800

    938 cpp

--- complete/938.range-sum-of-bst.c
+++ complete/938.range-sum-of-bst.c
@@ -13,21 +13,34 @@
  *     struct TreeNode *right;
  * };
  */
-void traveler(struct TreeNode* root, int low, int high, int *sum){
+// Solution 2:
+int rangeSumBST(struct TreeNode* root, int low, int high) {
     if(!root)
         return 0;
-    if(root->val >= low && root->val <= high)
-        *sum += root->val;
-    if(root->left)
-            traveler(root->left, low,  high, sum);
-    if(root->right)
-            traveler(root->right, low,  high, sum);
-}
-
-int rangeSumBST(struct TreeNode* root, int low, int high){
-    int sum = 0;
-    traveler( root,  low,  high, &sum);
-    return sum;
+    if(root->val > high)
+        return rangeSumBST(root->left, low, high);
+    else if(root->val < low)
+        return rangeSumBST(root->right, low, high);
+    return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
 }
 // @lc code=end
 
+// Note: BST
+
+// Solution 1:
+// void traveler(struct TreeNode* root, int low, int high, int *sum){
+//     if(!root)
+//         return 0;
+//     if(root->val >= low && root->val <= high)
+//         *sum += root->val;
+//     if(root->left)
+//             traveler(root->left, low,  high, sum);
+//     if(root->right)
+//             traveler(root->right, low,  high, sum);
+// }
+
+// int rangeSumBST(struct TreeNode* root, int low, int high){
+//     int sum = 0;
+//     traveler( root,  low,  high, &sum);
+//     return sum;
+// }
