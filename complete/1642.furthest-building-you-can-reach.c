547c9eabbc65ecd477e7686b971a688c1db56406
Ted Chang <student0804@gmail.com>
2024-02-18 14:11:15 +0800

    1642

---
+++ complete/1642.furthest-building-you-can-reach.c
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode id=1642 lang=c
+ *
+ * [1642] Furthest Building You Can Reach
+ */
+
+// @lc code=start
+typedef struct
+{
+    int size;
+    int *arr;
+} HEAP;
+
+HEAP *init_heap(int n)
+{
+    HEAP *obj = malloc(sizeof(HEAP));
+    obj->size = 0;
+    obj->arr = calloc(n, sizeof(int));
+
+    return obj;
+}
+
+void free_heap(HEAP *obj)
+{
+    free(obj->arr);
+    free(obj);
+}
+
+void push_max_heap(int val, HEAP *obj)
+{
+    obj->arr[obj->size++] = val;
+    int lower = obj->size - 1;
+    int upper = (lower + 1) / 2 - 1;
+    int tmp;
+    while (upper >= 0)
+    {
+        if (lower + 1 < obj->size && obj->arr[lower + 1] > obj->arr[lower])
+            lower++;
+        if (obj->arr[lower] < obj->arr[upper])
+            break;
+        if (obj->arr[upper] < obj->arr[lower])
+        {
+            tmp = obj->arr[upper];
+            obj->arr[upper] = obj->arr[lower];
+            obj->arr[lower] = tmp;
+        }
+        lower = upper;
+        upper = (lower + 1) / 2 - 1;
+    }
+}
+
+int pop_max_heap(HEAP *obj)
+{
+    int r = obj->arr[0];
+    obj->arr[0] = obj->arr[--obj->size];
+    int n = obj->size;
+    int upper = 0;
+    int lower = 2 * upper + 1;
+    int tmp;
+    while (lower < n)
+    {
+        if (lower + 1 < n && obj->arr[lower + 1] > obj->arr[lower])
+            lower++;
+        if (obj->arr[lower] < obj->arr[upper])
+            break;
+        if (obj->arr[upper] < obj->arr[lower])
+        {
+            tmp = obj->arr[upper];
+            obj->arr[upper] = obj->arr[lower];
+            obj->arr[lower] = tmp;
+        }
+        upper = lower;
+        lower = upper * 2 + 1;
+    }
+    return r;
+}
+
+int furthestBuilding(int *heights, int heightsSize, int bricks, int ladders)
+{
+    HEAP *h = init_heap(heightsSize);
+    int gap;
+    for (int i = 1; i < heightsSize; i++)
+    {
+        gap = heights[i] - heights[i - 1];
+        if (gap <= 0)
+            continue;
+        if (gap <= bricks)
+        {
+            push_max_heap(gap, h);
+            bricks -= gap;
+        }
+        else
+        {
+            push_max_heap(gap, h);
+            bricks -= gap;
+            while (ladders && bricks < 0)
+            {
+                bricks += pop_max_heap(h);
+                ladders--;
+            }
+            if (bricks < 0)
+            {
+                free_heap(h);
+                return i - 1;
+            }
+        }
+    }
+    free_heap(h);
+    return heightsSize - 1;
+}
+// @lc code=end
+
+// Note: greedy + heap
