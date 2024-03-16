07bf523623ae58702f68f789676e75d61b7d5f8d
Ted Chang <student0804@gmail.com>
2024-03-16 17:45:24 +0800

    525 cpp

---
+++ complete/525.contiguous-array.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=525 lang=cpp
+ *
+ * [525] Contiguous Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void count(int range, vector<int> &nums, vector<int> &r)
+    {
+        r[0] = 0;
+        for (int i = 0; i < range; i++)
+        {
+            if (nums[i] == 0)
+                r[i + 1] = r[i] + 1;
+            else
+                r[i + 1] = r[i] - 1;
+        }
+    }
+    int findMaxLength(vector<int> &nums)
+    {
+        int numsSize = nums.size();
+        vector<int> r(numsSize + 1);
+        count(numsSize, nums, r);
+
+        vector<int> max_j_arr(numsSize * 2 + 1, -1);
+        for (int i = 0; i < numsSize * 2 + 1; i++)
+            max_j_arr[i] = -1;
+
+        for (int i = 0; i < numsSize + 1; i++)
+            max_j_arr[r[i] + numsSize] = i;
+        int max_L = 0;
+        int target;
+        int max_j;
+        int temp_length;
+        for (int i = 1; i < numsSize; i++)
+        {
+            target = r[i - 1];
+            max_j = max_j_arr[target + numsSize];
+
+            temp_length = max_j - i + 1;
+            if (max_L < temp_length)
+                max_L = temp_length;
+        }
+
+        return max_L;
+    }
+};
+// @lc code=end
+
+// NOTE
+// count(i, j, nums) == count(0, j, nums) - count(0, i-1, nums)
+// -> count(0, j, nums) == count(0, i-1, nums)
+// 機率累積圖
