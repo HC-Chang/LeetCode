d85a8666c11e996ba540817c1f7dce0793b6846a
Ted Chang <student0804@gmail.com>
2024-05-11 08:33:37 +0800

    786

---
+++ complete/786.k-th-smallest-prime-fraction.c
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=786 lang=c
+ *
+ * [786] K-th Smallest Prime Fraction
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct
+{
+    int v1;
+    int v2;
+} DATA;
+int sort(void *a, void *b) { return ((DATA *)a)->v1 * ((DATA *)b)->v2 - ((DATA *)b)->v1 * ((DATA *)a)->v2; }
+int *kthSmallestPrimeFraction(int *arr, int arrSize, int k, int *returnSize)
+{
+    DATA *d = calloc(arrSize * (arrSize - 1) / 2, sizeof(DATA));
+    int idx = 0;
+    for (int i = 0; i < arrSize; i++)
+    {
+        for (int j = i + 1; j < arrSize; j++)
+        {
+            d[idx].v1 = arr[i];
+            d[idx++].v2 = arr[j];
+        }
+    }
+    qsort(d, idx, sizeof(DATA), sort);
+
+    *returnSize = 2;
+    arr[0] = d[k - 1].v1;
+    arr[1] = d[k - 1].v2;
+    free(d);
+    return arr;
+}
+// @lc code=end
+
+// Note: sorting / heap
