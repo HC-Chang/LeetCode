39a19fcebd12242b2d7f281b9377dfaee87a8d12
Ted Chang <student0804@gmail.com>
2024-06-04 22:36:14 +0800

    409 cpp

---
+++ complete/409.longest-palindrome.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=409 lang=c
+ *
+ * [409] Longest Palindrome
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int longestPalindrome(string s)
+    {
+        int map[60] = {0};
+
+        for (auto c : s)
+            ++map[c - 'A'];
+
+        int r = 0;
+        int temp_len;
+        for (int i = 0; i < 60; ++i)
+        {
+            temp_len = map[i];
+            if (map[i] % 2 == 1)
+                temp_len--;
+
+            r += temp_len;
+            if (r % 2 == 0 && map[i] % 2 == 1)
+                r++;
+        }
+
+        return r;
+    }
+};
+// @lc code=end
