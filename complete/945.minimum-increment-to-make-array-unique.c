a3a002bbbb9f1ebe5cf9caee1bb9ef423aac9773
Ted Chang <student0804@gmail.com>
2024-06-14 21:54:10 +0800

    modify 945

--- complete/945.minimum-increment-to-make-array-unique.c
+++ complete/945.minimum-increment-to-make-array-unique.c
@@ -5,28 +5,51 @@
  */
 
 // @lc code=start
-#define SIZE 101001
+// Solution 2:
+
+int sort(void *a, void *b) { return *(int *)a - *(int *)b; }
 int minIncrementForUnique(int *nums, int numsSize)
 {
-    int hash[SIZE] = {0};
+    qsort(nums, numsSize, sizeof(int), sort);
     int cnt = 0;
-    int offset = 1;
-    for (int i = 0; i < numsSize; i++)
+    int last = nums[0];
+    for (int i = 1; i < numsSize; ++i)
     {
-        if (!hash[nums[i]])
-            ++hash[nums[i]];
-        else
+        if (nums[i] <= last)
         {
-            while (nums[i] + offset < SIZE - 1 && (hash[nums[i] + offset]) > 0)
-                offset++;
-            hash[nums[i] + offset] = 1;
-            cnt += offset;
-            offset = 1;
+            cnt += last - nums[i] + 1;
+            last++;
         }
+        else
+            last = nums[i];
     }
-
     return cnt;
 }
 // @lc code=end
 
 // Note: sorting + counting
+
+// Solution 1:
+
+// #define SIZE 101001
+// int minIncrementForUnique(int *nums, int numsSize)
+// {
+//     int hash[SIZE] = {0};
+//     int cnt = 0;
+//     int offset = 1;
+//     for (int i = 0; i < numsSize; i++)
+//     {
+//         if (!hash[nums[i]])
+//             ++hash[nums[i]];
+//         else
+//         {
+//             while (nums[i] + offset < SIZE - 1 && (hash[nums[i] + offset]) > 0)
+//                 offset++;
+//             hash[nums[i] + offset] = 1;
+//             cnt += offset;
+//             offset = 1;
+//         }
+//     }
+
+//     return cnt;
+// }
--- complete/945.minimum-increment-to-make-array-unique.cpp
+++ complete/945.minimum-increment-to-make-array-unique.cpp
@@ -5,34 +5,61 @@
  */
 
 // @lc code=start
-#define SIZE 101001
+// Solution 2:
+
 class Solution
 {
 public:
     int minIncrementForUnique(vector<int> &nums)
     {
-        int hash[SIZE] = {0};
+        sort(nums.begin(), nums.end());
         int cnt = 0;
-        int offset = 1;
+        int last = nums[0];
         int n = nums.size();
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            if (!hash[nums[i]])
-                ++hash[nums[i]];
-            else
+            if (nums[i] <= last)
             {
-                while (nums[i] + offset < SIZE - 1 &&
-                       (hash[nums[i] + offset]) > 0)
-                    offset++;
-                hash[nums[i] + offset] = 1;
-                cnt += offset;
-                offset = 1;
+                cnt += last - nums[i] + 1;
+                last++;
             }
+            else
+                last = nums[i];
         }
-
         return cnt;
     }
 };
 // @lc code=end
 
 // Note: sorting + counting
+
+// Solution 1:
+
+// #define SIZE 101001
+// class Solution
+// {
+// public:
+//     int minIncrementForUnique(vector<int> &nums)
+//     {
+//         int hash[SIZE] = {0};
+//         int cnt = 0;
+//         int offset = 1;
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (!hash[nums[i]])
+//                 ++hash[nums[i]];
+//             else
+//             {
+//                 while (nums[i] + offset < SIZE - 1 &&
+//                        (hash[nums[i] + offset]) > 0)
+//                     offset++;
+//                 hash[nums[i] + offset] = 1;
+//                 cnt += offset;
+//                 offset = 1;
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
