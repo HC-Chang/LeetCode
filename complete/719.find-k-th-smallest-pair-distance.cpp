abc0b092e0c611f0029a638aece1847159d59dfc
Ted Chang <student0804@gmail.com>
2024-08-14 23:07:36 +0800

    719 cpp

---
+++ complete/719.find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=719 lang=cpp
+ *
+ * [719] Find K-th Smallest Pair Distance
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int smallestDistancePair(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        int arraySize = nums.size();
+
+        int low = 0;
+        int high = nums[arraySize - 1] - nums[0];
+
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+
+            int count = countPairsWithMaxDistance(nums, mid);
+
+            if (count < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+
+private:
+    int countPairsWithMaxDistance(vector<int> &nums, int maxDistance)
+    {
+        int count = 0;
+        int arraySize = nums.size();
+        int left = 0;
+
+        for (int right = 0; right < arraySize; ++right)
+        {
+            while (nums[right] - nums[left] > maxDistance)
+                ++left;
+
+            count += right - left;
+        }
+        return count;
+    }
+};
+// @lc code=end
+
+// Note: binary search + sliding window
+
+// Official Solution
