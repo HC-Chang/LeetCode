70e79fdc12bf680fc0592c0624fd53288102d763
Ted Chang <student0804@gmail.com>
2024-09-09 00:16:39 +0800

    modify 725 cpp

--- complete/725.split-linked-list-in-parts.cpp
+++ complete/725.split-linked-list-in-parts.cpp
@@ -15,34 +15,82 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// Solution 2:
+
 class Solution
 {
 public:
     vector<ListNode *> splitListToParts(ListNode *head, int k)
     {
         ListNode *cur;
-        int len = 0;
+        vector<ListNode *> arr;
         for (cur = head; cur; cur = cur->next)
-            ++len;
+            arr.push_back(cur);
+
+        int n = arr.size();
+        int q = n / k;
+        int r = n % k;
+
         vector<ListNode *> ans;
-        cur = head;
-        int l = len / k;
-        int r = len % k;
-        ListNode *prev = NULL;
-        for (int i = 0; i < k; ++i, --r)
+        if (n <= k)
         {
-            ans.push_back(cur);
-            for (int j = 0; j < l + (r > 0); ++j)
+            for (int i = 0; i < n; i++)
             {
-                prev = cur;
-                cur = cur->next;
+                ans.push_back(arr[i]);
+                arr[i]->next = nullptr;
             }
-            if (prev)
-                prev->next = NULL;
+            for (int i = 0; i < k - n; i++)
+                ans.push_back({});
+            return ans;
         }
+
+        int idx = 0;
+        int last = 0;
+        for (int i = 0; i < k; i++)
+        {
+            if (i <= r - 1)
+                last = min(idx + q, n - 1);
+            else
+                last = min(idx + q - 1, n - 1);
+            ans.push_back(arr[idx]);
+            arr[last]->next = nullptr;
+            idx = last + 1;
+        }
+
         return ans;
     }
 };
 // @lc code=end
 
 // Note: link-list
+
+// Solution 1:
+
+// class Solution
+// {
+// public:
+//     vector<ListNode *> splitListToParts(ListNode *head, int k)
+//     {
+//         ListNode *cur;
+//         int len = 0;
+//         for (cur = head; cur; cur = cur->next)
+//             ++len;
+//         vector<ListNode *> ans;
+//         cur = head;
+//         int l = len / k;
+//         int r = len % k;
+//         ListNode *prev = NULL;
+//         for (int i = 0; i < k; ++i, --r)
+//         {
+//             ans.push_back(cur);
+//             for (int j = 0; j < l + (r > 0); ++j)
+//             {
+//                 prev = cur;
+//                 cur = cur->next;
+//             }
+//             if (prev)
+//                 prev->next = NULL;
+//         }
+//         return ans;
+//     }
+// };
