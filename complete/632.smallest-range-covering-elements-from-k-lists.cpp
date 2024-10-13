7e7178de74d22174bab3fa68a7381e731816c68a
Ted Chang <student0804@gmail.com>
2024-10-13 10:09:20 +0800

    632 cpp

---
+++ complete/632.smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=632 lang=c
+ *
+ * [632] Smallest Range Covering Elements from K Lists
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> smallestRange(vector<vector<int>> &nums)
+    {
+        vector<pair<int, int>> merged;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int num : nums[i])
+                merged.push_back({num, i});
+        }
+
+        sort(merged.begin(), merged.end());
+
+        unordered_map<int, int> freq;
+        int left = 0, count = 0;
+        int rangeStart = 0, rangeEnd = INT_MAX;
+
+        for (int right = 0; right < merged.size(); right++)
+        {
+            freq[merged[right].second]++;
+            if (freq[merged[right].second] == 1)
+                count++;
+
+            while (count == nums.size())
+            {
+                int curRange = merged[right].first - merged[left].first;
+                if (curRange < rangeEnd - rangeStart)
+                {
+                    rangeStart = merged[left].first;
+                    rangeEnd = merged[right].first;
+                }
+
+                freq[merged[left].second]--;
+                if (freq[merged[left].second] == 0)
+                    count--;
+                left++;
+            }
+        }
+
+        return {rangeStart, rangeEnd};
+    }
+};
+// @lc code=end
+
+// Note:
+
+// Official Solution
