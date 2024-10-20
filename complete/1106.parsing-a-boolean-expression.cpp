06620434a144e4caffefc51845e70b8b83224bad
Ted Chang <student0804@gmail.com>
2024-10-20 10:58:55 +0800

    1106 cpp

---
+++ complete/1106.parsing-a-boolean-expression.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=1106 lang=cpp
+ *
+ * [1106] Parsing A Boolean Expression
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool parseBoolExpr(string expression)
+    {
+        stack<char> st;
+
+        for (char currChar : expression)
+        {
+            if (currChar == ',' || currChar == '(')
+                continue;
+
+            if (currChar == 't' || currChar == 'f' || currChar == '!' ||
+                currChar == '&' || currChar == '|')
+                st.push(currChar);
+
+            else if (currChar == ')')
+            {
+                bool hasTrue = false, hasFalse = false;
+
+                while (st.top() != '!' && st.top() != '&' && st.top() != '|')
+                {
+                    char topValue = st.top();
+                    st.pop();
+                    if (topValue == 't')
+                        hasTrue = true;
+                    if (topValue == 'f')
+                        hasFalse = true;
+                }
+
+                char op = st.top();
+                st.pop();
+                if (op == '!')
+                    st.push(hasTrue ? 'f' : 't');
+                else if (op == '&')
+                    st.push(hasFalse ? 'f' : 't');
+                else
+                    st.push(hasTrue ? 't' : 'f');
+            }
+        }
+
+        return st.top() == 't';
+    }
+};
+// @lc code=end
+
+// Note: stack + recursion
+
+// Officail Solution
