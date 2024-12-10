6df230fcc6b6aec9cc73b0ccb1d0a8e21b8e9649
Ted Chang <student0804@gmail.com>
2024-12-10 23:57:32 +0800

    2981

---
+++ complete/2981.find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=2981 lang=cpp
+ *
+ * [2981] Find Longest Special Substring That Occurs Thrice I
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int maximumLength(string s)
+    {
+        map<pair<char, int>, int> count;
+        int substringLength = 0;
+        for (int start = 0; start < s.length(); start++)
+        {
+            char character = s[start];
+            substringLength = 0;
+            for (int end = start; end < s.length(); end++)
+            {
+                if (character == s[end])
+                {
+                    substringLength++;
+                    count[{character, substringLength}]++;
+                }
+                else
+                    break;
+            }
+        }
+
+        int ans = 0;
+        for (auto i : count)
+        {
+            int len = i.first.second;
+            if (i.second >= 3 && len > ans)
+                ans = len;
+        }
+        if (ans == 0)
+            return -1;
+        return ans;
+    }
+};
+// @lc code=end
+
+// Note: hash table + sliding window
+
+// Official Solution
