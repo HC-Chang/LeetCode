84f4ef81ff67ad79845af668e8a5882b0b0068fb
Ted Chang <student0804@gmail.com>
2025-03-05 23:37:59 +0800

    2579

---
+++ complete/2579.count-total-number-of-colored-cells.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=2579 lang=cpp
+ *
+ * [2579] Count Total Number of Colored Cells
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    long long coloredCells(int n)
+    {
+        return (long long)2 * n * n - 2 * n + 1;
+    }
+};
+// @lc code=end
+
+// Note: math
