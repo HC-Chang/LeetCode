f689b95765423a656c47141a6f899f648f64843b
Ted Chang <student0804@gmail.com>
2025-04-13 10:57:41 +0800

    1922

---
+++ complete/1922.count-good-numbers.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1922 lang=cpp
+ *
+ * [1922] Count Good Numbers
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    const long long MOD = 1000000007;
+    long long quickmul(int x, long long y)
+    {
+        long long ret = 1;
+        long long mul = x;
+        while (y > 0)
+        {
+            if (y % 2 == 1)
+                ret = (ret * mul) % MOD;
+
+            mul = (mul * mul) % MOD;
+            y /= 2;
+        }
+        return ret;
+    }
+
+    int countGoodNumbers(long long n)
+    {
+        return (int)(quickmul(5, (n + 1) / 2) * quickmul(4, n / 2) % MOD);
+    }
+};
+// @lc code=end
+
+// Note: math + recursion
+
+// Official Solution
