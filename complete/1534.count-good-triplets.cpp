a2588436089c630152d538eb2c1925c44d4c731c
Ted Chang <student0804@gmail.com>
2025-04-14 20:26:51 +0800

    1534

---
+++ complete/1534.count-good-triplets.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1534 lang=cpp
+ *
+ * [1534] Count Good Triplets
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countGoodTriplets(vector<int> &arr, int a, int b, int c)
+    {
+        int n = arr.size();
+        int cnt = 0;
+        for (int i = 0; i < n - 2; i++)
+        {
+            for (int j = i + 1; j < n - 1; j++)
+            {
+                if (abs(arr[i] - arr[j]) > a)
+                    continue;
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (abs(arr[j] - arr[k]) > b)
+                        continue;
+                    if (abs(arr[i] - arr[k]) <= c)
+                        ++cnt;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+// @lc code=end
