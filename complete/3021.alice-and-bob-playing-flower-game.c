0112a4345fc98ce487d325793b382547de982dee
Ted Chang <student0804@gmail.com>
2025-08-29 21:47:52 +0800

    3021

---
+++ complete/3021.alice-and-bob-playing-flower-game.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3021 lang=c
+ *
+ * [3021] Alice and Bob Playing Flower Game
+ */
+
+// @lc code=start
+long long flowerGame(int n, int m)
+{
+    return (long long)m * n / 2;
+}
+// @lc code=end
+
+// Note: math
---
+++ complete/3021.alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=3021 lang=cpp
+ *
+ * [3021] Alice and Bob Playing Flower Game
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    long long flowerGame(int n, int m)
+    {
+        return (long long)m * n / 2;
+    }
+};
+// @lc code=end
+
+// Note: math
