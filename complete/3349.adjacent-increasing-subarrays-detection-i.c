e5aacbfaff2b7b9f415c9ce2f86b2398c06b2d53
Ted Chang <student0804@gmail.com>
2025-10-15 21:37:58 +0800

    3349

---
+++ complete/3349.adjacent-increasing-subarrays-detection-i.c
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=3349 lang=c
+ *
+ * [3349] Adjacent Increasing Subarrays Detection I
+ */
+
+// @lc code=start
+bool find_k(int idx, int k, int *nums)
+{
+    for (int i = 1; i < k; i++)
+    {
+        if (nums[idx + i] <= nums[idx + i - 1])
+            return false;
+    }
+    return true;
+}
+
+bool hasIncreasingSubarrays(int *nums, int numsSize, int k)
+{
+    if (k == 1)
+        return true;
+    int offset;
+    bool b1, b2;
+    for (int i = 0; i <= numsSize - 2 * k; i++)
+    {
+        offset = 0;
+        b1 = find_k(i, k, nums);
+        b2 = find_k(i + k, k, nums);
+        if (b1 && b2)
+            return true;
+        if (b1)
+            offset += k;
+        if (b2)
+            offset += k;
+    }
+    return false;
+}
+// @lc code=end
