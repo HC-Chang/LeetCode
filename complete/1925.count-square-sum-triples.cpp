13a5b3b7710224771df892f206d319476eca5865
Ted Chang <student0804@gmail.com>
2025-12-09 01:10:57 +0800

    1925

---
+++ complete/1925.count-square-sum-triples.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=1925 lang=cpp
+ *
+ * [1925] Count Square Sum Triples
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countTriples(int n)
+    {
+        int cnt = 0;
+        int cache[n + 1];
+        for (int i = 1; i <= n; i++)
+            cache[i] = i * i;
+
+        for (int k = 5; k <= n; k++)
+        {
+            for (int i = 3; i <= k; i++)
+            {
+                for (int j = 3; j <= k; j++)
+                {
+                    if (cache[i] + cache[j] == cache[k])
+                        cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+// @lc code=end
+
+// Note: math + enumeration
