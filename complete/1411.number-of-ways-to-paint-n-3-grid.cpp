0f375fb83223da3fb4c992ded3a8051f46146bc4
Ted Chang <student0804@gmail.com>
2026-01-03 15:12:30 +0800

    1411

---
+++ complete/1411.number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1411 lang=cpp
+ *
+ * [1411] Number of Ways to Paint N × 3 Grid
+ */
+
+// @lc code=start
+#define MOD (long)(1e9 + 7)
+
+class Solution
+{
+public:
+    int numOfWays(int n)
+    {
+        long two = 6, three = 6;
+        if (n == 1)
+            return 12;
+        long ntwo;
+        long nthree;
+        for (int i = 2; i <= n; ++i)
+        {
+            ntwo = (3 * two + 2 * three) % MOD;
+            nthree = (2 * two + 2 * three) % MOD;
+            two = ntwo;
+            three = nthree;
+        }
+        return (int)((two + three) % MOD);
+    }
+};
+// @lc code=end
+
+// Note: DP
