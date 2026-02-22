7c99e6732fa05dd55531388254392bf22f4baca5
Ted Chang <student0804@gmail.com>
2026-02-22 10:43:36 +0800

    868

---
+++ complete/868.binary-gap.c
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=868 lang=c
+ *
+ * [868] Binary Gap
+ */
+
+// @lc code=start
+int binaryGap(int n)
+{
+    int cnt = 0;
+    int tmp = 0;
+    int flag = 0;
+    while (n > 0)
+    {
+        if ((n & 1) == 0)
+        {
+            if (flag)
+                tmp++;
+        }
+        else
+        {
+            if (flag && tmp >= cnt)
+                cnt = ++tmp;
+            flag = 1;
+            tmp = 0;
+        }
+        n >>= 1;
+    }
+    return cnt;
+}
+// @lc code=end
+
+// NOTE: bit manipulation
