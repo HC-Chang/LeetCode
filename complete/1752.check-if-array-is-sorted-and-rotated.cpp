18e1387b2568886332387bfa9d11c8c06c26b046
Ted Chang <student0804@gmail.com>
2026-05-23 09:17:27 +0800

    update 1752 cpp

--- complete/1752.check-if-array-is-sorted-and-rotated.cpp
+++ complete/1752.check-if-array-is-sorted-and-rotated.cpp
@@ -5,31 +5,61 @@
  */
 
 // @lc code=start
+
+// Solution 2: O(n) time, O(1) space
+
 class Solution
 {
 public:
     bool check(vector<int> &nums)
     {
+        bool flag = false;
         int n = nums.size();
-        vector<pair<int, int>> p;
-        for (int i = 0; i < n; i++)
-            p.push_back({nums[i], i});
-        sort(p.begin(), p.end());
-
-        int start = p[0].second;
-        while (n > 0 && nums[(start - 1 + n) % n] == nums[(start) % n])
+        for (int i = 1; i < n; i++)
         {
-            start = (start - 1 + n) % n;
-            n--;
+            if (nums[i] < nums[i - 1])
+            {
+                if (flag || (i + 1 < n && nums[i - 1] < nums[i + 1]))
+                    return false;
+                else
+                    flag = true;
+            }
         }
 
-        for (int i = 0; i < n - 1; i++)
-        {
-            if (nums[(start + 1 + i) % n] - nums[(start + i) % n] < 0)
-                return false;
-        }
+        if (flag && nums[0] < nums[n - 1])
+            return false;
 
         return true;
     }
 };
 // @lc code=end
+
+// Solution 1: O(nlogn) time, O(n) space
+
+// class Solution
+// {
+// public:
+//     bool check(vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         vector<pair<int, int>> p;
+//         for (int i = 0; i < n; i++)
+//             p.push_back({nums[i], i});
+//         sort(p.begin(), p.end());
+
+//         int start = p[0].second;
+//         while (n > 0 && nums[(start - 1 + n) % n] == nums[(start) % n])
+//         {
+//             start = (start - 1 + n) % n;
+//             n--;
+//         }
+
+//         for (int i = 0; i < n - 1; i++)
+//         {
+//             if (nums[(start + 1 + i) % n] - nums[(start + i) % n] < 0)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// };
