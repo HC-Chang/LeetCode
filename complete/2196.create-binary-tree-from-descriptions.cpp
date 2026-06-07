10a12e0e83019879b9cb4ff3b832a55384c340da
Ted Chang <student0804@gmail.com>
2026-06-07 08:25:09 +0800

    2196 cpp update

--- complete/2196.create-binary-tree-from-descriptions.cpp
+++ complete/2196.create-binary-tree-from-descriptions.cpp
@@ -13,8 +13,7 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution
@@ -22,40 +21,23 @@ class Solution
 public:
     TreeNode *createBinaryTree(vector<vector<int>> &descriptions)
     {
-        unordered_map<int, bool> isRoot;
-        unordered_map<int, TreeNode *> nodes;
-
-        for (const auto &d : descriptions)
+        unordered_map<int, TreeNode *> hash;
+        for (auto &d : descriptions)
         {
-            int p = d[0];
-            int c = d[1];
-            bool left = d[2];
-            if (!isRoot.count(p))
-                isRoot[p] = true;
-
-            isRoot[c] = false;
-            if (!nodes.count(p))
-                nodes[p] = new TreeNode(p);
-
-            if (!nodes.count(c))
-                nodes[c] = new TreeNode(c);
-
-            if (left)
-                nodes[p]->left = nodes[c];
+            if (hash.find(d[0]) == hash.end())
+                hash[d[0]] = new TreeNode(d[0]);
+            if (hash.find(d[1]) == hash.end())
+                hash[d[1]] = new TreeNode(d[1]);
+            if (d[2])
+                hash[d[0]]->left = hash[d[1]];
             else
-                nodes[p]->right = nodes[c];
+                hash[d[0]]->right = hash[d[1]];
         }
 
-        int root = -1;
-        for (const auto &[val, r] : isRoot)
-        {
-            if (r)
-            {
-                root = val;
-                break;
-            }
-        }
-        return nodes[root];
+        for (auto &d : descriptions)
+            hash.erase(d[1]);
+
+        return hash.begin()->second;
     }
 };
 // @lc code=end
