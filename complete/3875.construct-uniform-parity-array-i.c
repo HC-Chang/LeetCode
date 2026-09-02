4b74b7e75fab1d372219ee8fa08af5f98cd73e27
Ted Chang <student0804@gmail.com>
2026-09-02 10:27:56 +0800

    3875

---
+++ complete/3875.construct-uniform-parity-array-i.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3875 lang=c
+ *
+ * [3875] Construct Uniform Parity Array I
+ */
+
+// @lc code=start
+bool uniformArray(int *nums1, int nums1Size)
+{
+    return true;
+}
+// @lc code=end
+
+// Note: math
---
+++ complete/3875.construct-uniform-parity-array-i.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=3875 lang=cpp
+ *
+ * [3875] Construct Uniform Parity Array I
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool uniformArray(vector<int> &nums1)
+    {
+        return true;
+    }
+};
+// @lc code=end
+
+// Note: math
