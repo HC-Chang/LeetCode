8c1824d942981461c02b26a160b19ce98020a34e
Ted Chang <student0804@gmail.com>
2026-09-08 23:38:17 +0800

    3870

---
+++ complete/3870.count-commas-in-range.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=3870 lang=c
+ *
+ * [3870] Count Commas in Range
+ */
+
+// @lc code=start
+int countCommas(int n)
+{
+    n -= 999;
+    if (n < 0)
+        return 0;
+    return n;
+}
+// @lc code=end
+
+// Note: math
---
+++ complete/3870.count-commas-in-range.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=3870 lang=cpp
+ *
+ * [3870] Count Commas in Range
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countCommas(int n)
+    {
+        n -= 999;
+        if (n < 0)
+            return 0;
+        return n;
+    }
+};
+// @lc code=end
+
+// Note: math
